cc37fd5b5d73cd52310cb56b61f3ed3c3865a1cf
kimyou000 <88136923+kimyou000@users.noreply.github.com>
2022-12-11 20:20:21 +0900

    Update Hotsource_Image.py

--- Hotsource_Image.py
+++ Hotsource_Image.py
@@ -1,3 +1,16 @@
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
+from PIL import Image
+
+#성수
+imageA = cv2.imread('./image/test1-1.jpg')
+imageB = cv2.imread('./image/test1-2.jpg')
+
+print(imageA.shape)
+print(imageB.shape)
+
+h, w, c = imageA.shape
+
+imageB = cv2.resize(imageB, (w, h))
+print(imageB.shape) 
